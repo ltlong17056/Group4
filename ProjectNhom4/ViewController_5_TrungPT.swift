e03d66c77b5b6de53b83f254da8e25056f35b4e9
cuscedu <cuscedu@mac08.local>
2017-05-26 16:17:13 +0700

    First_create_project

---
+++ ProjectNhom4/ViewController_5_TrungPT.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController_5_TrungPT.swift
+//  ProjectNhom4
+//
+//  Created by cuscedu on 5/26/17.
+//  Copyright © 2017 cusc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController_5_TrungPT: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
